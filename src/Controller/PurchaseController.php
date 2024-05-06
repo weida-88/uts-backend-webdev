1a4ac919c2c2fa61a7a529dd78aa9f7d30868486
weida-88 <230010002@stikom-bali.ac.id>
2024-05-06 22:24:54 +0800

    testing commit 2

--- src/Controller/PurchaseController.php
+++ src/Controller/PurchaseController.php
@@ -1,15 +1,1 @@
-<?php
 
-class PurchasesController {
-    public function index() {
-        // Logika untuk menampilkan data pembelian
-        echo "Menampilkan data pembelian";
-    }
-
-    public function show($id) {
-        // Logika untuk menampilkan detail pembelian berdasarkan ID
-        echo "Menampilkan detail pembelian dengan ID: $id";
-    }
-
-    // Tambahkan metode lainnya sesuai kebutuhan
-}
